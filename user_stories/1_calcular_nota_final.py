64624fa682dab20a7d7da1c5aae22e68d576a91d
mkonstanti <71271552+mkonstanti@users.noreply.github.com>
2025-02-23 18:43:13 +0100

    user story 1 correction

--- user_stories/1_calcular_nota_final.py
+++ user_stories/1_calcular_nota_final.py
@@ -1,14 +1,17 @@
 #Definir funcion para calcular la nota final del estudiante
-def calcular_nota_final(): 
+def calcular_nota_final(cantidad_notas): 
     nombre = input("Introduce el nombre del estudiante: ")
     suma = 0
-    for i in range(3): 
+    for i in range(cantidad_notas): 
         nota = float(input(f"Introduce la nota número {i+1} : "))
+        if nota < 0 or nota > 10:
+                print("Número inválido, la nota debe estar entre 0 y 10.")
+                return
         suma += nota
-    media_nota = suma / 3
+    nota_final = suma / cantidad_notas
     nombre_mayuscula = nombre.upper()
     #Mostrar la media nota en la consola
-    print(f"La nota final de {nombre_mayuscula} es {media_nota:.2f}")
+    print(f"La nota final de {nombre_mayuscula} es {nota_final:.2f}")
 
-#Llamamos la función
-calcular_nota_final()
+#Llamamos la función para calcular la media nota de 3 notas en total
+calcular_nota_final(3)
